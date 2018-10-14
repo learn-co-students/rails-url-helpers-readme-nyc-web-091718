5099158f6cfc8fd74af647de40936e5e19e3ad38
github username <github email address>
2018-10-13 23:09:54 -0400

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
   resources :posts, only: [:index, :show]
+  get '/register', to: 'users#new', as: 'register'
+  # The line immediately overhead customizes the path as "register" instead of "new".
 end
+
+
+# get '/register', to: 'users#new', as: 'register'
+# Now the application lets users navigate to /register to sign up, and you, the developer, can utilize your own custom register_path route helper throughout the app.
